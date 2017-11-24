0c6393f651bb0ba2e001306c25bbe218794b3c8f
Jim Shaw <jimxshaw@gmail.com>
2017-11-24 14:57:06 -0600

    Tests: add GetClient method in TestBase.

--- tests/ApiControllerTestBase.cs
+++ tests/ApiControllerTestBase.cs
@@ -1,14 +1,29 @@
-using System;
-using Xunit;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 
 namespace tests
 {
-    public class UnitTest1
+    public class ApiControllerTestBase
     {
-        [Fact]
-        public void Test1()
+        protected HttpClient GetClient()
         {
+            var builder = new WebHostBuilder()
+                                .UseContentRoot(Directory.GetCurrentDirectory())
+                                .UseStartup<Startup>()
+                                .UseEnvironment("Testing");
+            
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
 
+            // Client always expects json results.
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
         }
     }
 }
