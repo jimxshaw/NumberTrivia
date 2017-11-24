ad586f1abc05a49e86b5f049aec65c90ec0fbdd0
Jim Shaw <jimxshaw@gmail.com>
2017-11-24 13:26:50 -0600

    Api: add http get async action in TriviaController. Add TriviaResponse dto class.

---
+++ api/Controllers/TriviaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using api.Dto;
+using Newtonsoft.Json;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class TriviaController : Controller
+    {
+        [HttpGet("{number}")]
+        public async Task<TriviaResponse> GetAsync(int number) 
+        {
+            // Call numbersapi.com and return results.
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://numbersapi.com/" + number + "?json");
+            var triviaResult = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<TriviaResponse>(triviaResult);
+        }
+    }
+}
